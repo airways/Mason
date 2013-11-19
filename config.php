626449b8a465f1360c7d97a6567001ba6883c1f9
Isaac Raway <isaac@m.st>
2013-11-19 12:08:07 -0800

    Bump version to 1.01

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-// @version 1.00
+// @version 1.01
 // @element
 
-define('MASON_VERSION', '1.00');
+define('MASON_VERSION', '1.01');
 define('MASON_NAME', 'Mason');
 define('MASON_CLASS', 'Mason'); // must match module class name
 define('MASON_DESCRIPTION', 'Mason allows you to bind multiple Elements together into reusable Content Blocks which can be placed in a Content Elements field.');
